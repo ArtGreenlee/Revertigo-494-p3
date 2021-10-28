e92c3de50e1f1c815d0a72d7744f0d1f54adfde6
greenlar <53580725+ArtGreenlee@users.noreply.github.com>
2021-10-28 10:01:05 -0400

    Update EnemyStorage.cs

--- Assets/EnemyStorage.cs
+++ Assets/EnemyStorage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyStorage : MonoBehaviour
 {
     public HashSet<GameObject> enemies = new HashSet<GameObject>();
+
     public void addEnemy(GameObject enemyIn)
     {
         enemies.Add(enemyIn);
