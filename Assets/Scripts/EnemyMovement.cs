6e7c8cdde9f8a6a04e2e3db8d30c6a93db0a023e
greenlar <53580725+ArtGreenlee@users.noreply.github.com>
2021-10-30 15:34:08 -0400

    Added slowing tower, but its effect is not correctly applied

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,11 +8,18 @@ public class EnemyMovement : MonoBehaviour
     private int currentPointIndex;
     private List<Vector3> path;
     private Pathfinder pathFinder;
+    public float maxSpeed;
+    public float curSpeed;
+    public GameObject slowEffect;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         pathFinder = GameObject.Find("GameController").GetComponent<Pathfinder>();
+    }
+
+    void Start()
+    {
+        curSpeed = maxSpeed;
         path = new List<Vector3>();
         currentPointIndex = 1;
         List<List<Vector3>> pathIn = pathFinder.getPath();
@@ -23,10 +30,22 @@ public class EnemyMovement : MonoBehaviour
         nextPoint = path[currentPointIndex];
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            ShootsBullets stats = collision.gameObject.GetComponent<BulletController>().parent;
+            if (stats.slowsEnemy)
+            {
+                slowEnemy(stats.slowPercent, stats.slowDuration);
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += (nextPoint - transform.position).normalized * Time.deltaTime * 1;
+        transform.position += (nextPoint - transform.position).normalized * Time.deltaTime * curSpeed;
         if (Vector3.Distance(transform.position, nextPoint) < .1f)
         {
             if (currentPointIndex == path.Count - 1)
@@ -37,5 +56,20 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
+    public void slowEnemy(float slowPercentage, float slowDuration)
+    {
+        if (slowPercentage < (curSpeed / maxSpeed))
+        {
+            StartCoroutine(slowEnemyCoroutine(slowPercentage, slowDuration));
+        }
+    }
+
+    private IEnumerator slowEnemyCoroutine(float slowPercentage, float slowDuration)
+    {
+        Instantiate(slowEffect, transform.position, new Quaternion());
+        curSpeed *= slowPercentage;
+        yield return new WaitForSeconds(slowDuration);
+        curSpeed = maxSpeed;
+    }
 
 }
