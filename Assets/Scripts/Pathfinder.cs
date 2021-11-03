7dc24a8161f7ac4be496b7d49fd5ece4f7bf8240
Arthur Greenlee <greenzarthur@gmail.com>
2021-11-02 22:27:47 -0400

    yet another pathfinder upgrade

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -90,7 +90,321 @@ public class Pathfinder : MonoBehaviour
         else return path;
     }
 
-    public IEnumerator findPathBetweenPointsFast(Vector3 startVec, Vector3 endVec, int pathIndex)
+    /*public IEnumerator findPathBetweenPointsUber(Vector3 startVec, Vector3 endVec, int pathIndex)
+    {
+        finishedPaths[pathIndex] = false;
+
+        pos startOne = new pos(startVec);
+        pos endOne = new pos(endVec);
+        startOne.FCost = Vector3.Distance(startOne.v, endOne.v);
+        startOne.parent = startOne;
+        endOne.GCost = 0;
+
+        pos startTwo = new pos(endVec);
+        pos endTwo = new pos(startVec);
+
+
+        SortedList<float, pos> activePathOne = new SortedList<float, pos>();
+        HashSet<Vector3> activePathVectorsOne = new HashSet<Vector3>();
+        activePathOne.Add(startOne.FCost, startOne);
+        activePathVectorsOne.Add(startVec);
+        HashSet<Vector3> closedPathOne = new HashSet<Vector3>();
+        Dictionary<float, SortedList<float, pos>> FCostDuplicatesOne = new Dictionary<float, SortedList<float, pos>>();
+
+        SortedList<float, pos> activePathTwo = new SortedList<float, pos>();
+        HashSet<Vector3> activePathVectorsTwo = new HashSet<Vector3>();
+        activePathVectorsTwo.Add(endVec);
+        activePathTwo.Add(startTwo.FCost, startTwo);
+        HashSet<Vector3> closedPathTwo = new HashSet<Vector3>();
+        Dictionary<float, SortedList<float, pos>> FCostDuplicatesTwo = new Dictionary<float, SortedList<float, pos>>();
+
+
+        while (activePathOne.Count > 0 && activePathTwo.Count > 0)
+        {
+            pos curPosOne = activePathOne.Values[0];
+            if (FCostDuplicatesOne.ContainsKey(curPosOne.FCost))
+            {
+                curPosOne = FCostDuplicatesOne[curPosOne.FCost].Values[0];
+                FCostDuplicatesOne[curPosOne.FCost].RemoveAt(0);
+            }
+            activePathOne.RemoveAt(0);
+            activePathVectorsOne.Remove(curPosOne.v);
+            closedPathOne.Add(curPosOne.v);
+
+            pos curPosTwo = activePathTwo.Values[0];
+            if (FCostDuplicatesTwo.ContainsKey(curPosTwo.FCost))
+            {
+                curPosTwo = FCostDuplicatesTwo[curPosTwo.FCost].Values[0];
+                FCostDuplicatesTwo[curPosTwo.FCost].RemoveAt(0);
+            }
+            activePathTwo.RemoveAt(0);
+            activePathVectorsTwo.Remove(curPosTwo.v);
+            closedPathTwo.Add(curPosTwo.v);
+
+
+            if (curPosOne.v == curPosTwo.v)
+            {
+                path[pathIndex].Clear();
+                while (curPosOne.v != startVec)
+                {
+                    if (wallStorage.isWall(curPosOne.v))
+                    {
+                        //RERUN
+                        removeSegmentFromDictionary(pathIndex);
+                        removeVisualizerSegment(pathIndex);
+                        StartCoroutine(findPathBetweenPointsUber(startVec, endVec, pathIndex));
+                        yield break;
+                    }
+
+                    path[pathIndex].Insert(0, curPosOne.v);
+                    pathVectors[pathIndex].Add(curPosOne.v);
+                    if (enemyMovement == null)
+                    {
+                        visualizers[pathIndex].Add(Instantiate(pathFindingVisualizerSphere, curPosOne.v, new Quaternion()));
+                    }
+                    curPosOne = curPosOne.parent;
+                }
+
+                while (curPosTwo.v != endVec)
+                {
+                    if (wallStorage.isWall(curPosTwo.v))
+                    {
+                        //RERUN
+                        removeSegmentFromDictionary(pathIndex);
+                        removeVisualizerSegment(pathIndex);
+                        StartCoroutine(findPathBetweenPointsUber(startVec, endVec, pathIndex));
+                        yield break;
+                    }
+
+                    path[pathIndex].Add(curPosTwo.v);
+                    pathVectors[pathIndex].Add(curPosTwo.v);
+                    if (enemyMovement == null)
+                    {
+                        visualizers[pathIndex].Add(Instantiate(pathFindingVisualizerSphere, curPosTwo.v, new Quaternion()));
+                    }
+                    curPosTwo = curPosTwo.parent;
+                }
+                path[pathIndex].Add(endVec);
+
+                finishedPaths[pathIndex] = true;
+                numCoroutinesRunning--;
+                yield break;
+            }
+
+            foreach (Vector3 newVec in walkableTiles(curPosOne.v))
+            {
+                Instantiate(pathFindingVisualizerSphere, curPosOne.v, new Quaternion());
+                yield return new WaitForSeconds(.1f);
+                if (!closedPathOne.Contains(newVec) && !activePathVectorsOne.Contains(newVec))
+                {
+                    pos newPos = new pos(newVec);
+                    newPos.parent = curPosOne;
+                    float HCostTemp = Vector3.Distance(endVec, newVec);
+                    float GCostTemp = curPosOne.GCost + Vector3.Distance(newVec, curPosOne.v);
+                    float FCostTemp = GCostTemp + HCostTemp;
+                    newPos.HCost = HCostTemp;
+                    newPos.GCost = GCostTemp;
+                    newPos.FCost = FCostTemp;
+
+                    if (activePathOne.ContainsKey(newPos.FCost))
+                    {
+                        //FCOST DUPLICATE
+                        if (!FCostDuplicatesOne.ContainsKey(newPos.FCost))
+                        {
+                            SortedList<float, pos> tempSlist = new SortedList<float, pos>();
+                            tempSlist.Add(newPos.HCost, newPos);
+                            FCostDuplicatesOne.Add(newPos.FCost, tempSlist);
+                        }
+                        else
+                        {
+                            while (FCostDuplicatesOne[newPos.FCost].ContainsKey(newPos.HCost))
+                            {
+                                newPos.HCost -= .01f;
+                            }
+
+                            FCostDuplicatesOne[newPos.FCost].Add(newPos.HCost, newPos);
+
+                        }
+                    }
+                    else
+                    {
+                        activePathVectorsOne.Add(newVec);
+                        activePathOne.Add(newPos.FCost, newPos);
+                    }
+                }
+            }
+
+            foreach (Vector3 newVec in walkableTiles(curPosTwo.v))
+            {
+                Instantiate(pathFindingVisualizerSphere, curPosTwo.v, new Quaternion());
+                yield return new WaitForSeconds(.1f);
+                if (!closedPathTwo.Contains(newVec) && !activePathVectorsTwo.Contains(newVec))
+                {
+                    pos newPos = new pos(newVec);
+                    newPos.parent = curPosTwo;
+                    float HCostTemp = Vector3.Distance(startVec, newVec);
+                    float GCostTemp = curPosTwo.GCost + Vector3.Distance(newVec, curPosTwo.v);
+                    float FCostTemp = GCostTemp + HCostTemp;
+                    newPos.HCost = HCostTemp;
+                    newPos.GCost = GCostTemp;
+                    newPos.FCost = FCostTemp;
+
+                    if (activePathTwo.ContainsKey(newPos.FCost))
+                    {
+                        //FCOST DUPLICATE
+                        if (!FCostDuplicatesTwo.ContainsKey(newPos.FCost))
+                        {
+                            SortedList<float, pos> tempSlist = new SortedList<float, pos>();
+                            tempSlist.Add(newPos.HCost, newPos);
+                            FCostDuplicatesTwo.Add(newPos.FCost, tempSlist);
+                        }
+                        else
+                        {
+                            while (FCostDuplicatesTwo[newPos.FCost].ContainsKey(newPos.HCost))
+                            {
+                                newPos.HCost -= .01f;
+                            }
+                            FCostDuplicatesTwo[newPos.FCost].Add(newPos.HCost, newPos);
+                        }
+                    }
+                    else
+                    {
+                        activePathVectorsTwo.Add(newVec);
+                        activePathTwo.Add(newPos.FCost, newPos);
+                    }
+                }
+            }
+        }
+        if (enemyMovement == null)
+        {
+            wallStorage.removeMostRecentWall();
+        }
+        else
+        {
+            yield return new WaitForSeconds(.5f);
+        }
+        towerPlacer.shadowTower.transform.position = new Vector3(25, 0, 0);
+        numCoroutinesRunning++;
+        StartCoroutine(findPathBetweenPointsUber(startVec, endVec, pathIndex));
+        numCoroutinesRunning--;
+        yield break;    
+    }*/
+
+    public IEnumerator findPathBetweenPointsLyft(Vector3 startVec, Vector3 endVec, int pathIndex)
+    {
+        finishedPaths[pathIndex] = false;
+        
+        pos start = new pos(startVec);
+        pos end = new pos(endVec);
+        start.FCost = Vector3.Distance(start.v, end.v);
+        start.parent = start;
+        start.GCost = 0;
+
+        SortedList<float, pos> activePath = new SortedList<float, pos>();
+        HashSet<Vector3> activePathVectors = new HashSet<Vector3>();
+        activePath.Add(start.FCost, start);
+        activePathVectors.Add(start.v);
+        HashSet<Vector3> closedPath = new HashSet<Vector3>();
+
+        int speedSwitch = 0;
+        currentCount = 0;
+        speedThreshold = 1000;
+        while (activePath.Count > 0)
+        {
+            currentCount++;
+            speedSwitch++;
+            if (currentCount > speedThresholdTrigger)
+            {
+                currentCount = 0;
+                speedThreshold++;
+            }
+            if (speedSwitch > speedThreshold)
+            {
+                yield return new WaitForEndOfFrame();
+                speedSwitch = 0;
+            }
+            pos curPos = activePath.Values[0];
+            activePath.RemoveAt(0);
+            activePathVectors.Remove(curPos.v);
+            closedPath.Add(curPos.v);
+
+            foreach (Vector3 newVec in walkableTiles(curPos.v))
+            {
+                if (newVec == end.v)
+                {
+                    end.parent = curPos;
+                    List<Vector3> temp = new List<Vector3>();
+                    temp.Add(end.v);
+                    while (end.parent.v != start.v)
+                    {
+                        end = end.parent;
+                        temp.Add(end.v);
+                        pathVectors[pathIndex].Add(end.v);
+                        //only show visualizer if this is a main path
+                        if (enemyMovement == null)
+                        {
+                            visualizers[pathIndex].Add(Instantiate(pathFindingVisualizerSphere, end.v, new Quaternion()));
+                        }
+                        if (wallStorage.isWall(end.v))
+                        {
+                            //rerun this coroutine
+                            removeSegmentFromDictionary(pathIndex);
+                            removeVisualizerSegment(pathIndex);
+                            StartCoroutine(findPathBetweenPointsLyft(startVec, endVec, pathIndex));
+                            yield break;
+                        }
+                    }
+                    Debug.Log(currentCount);
+                    temp.Reverse();
+                    path[pathIndex] = temp;
+                    finishedPaths[pathIndex] = true;
+                    numCoroutinesRunning--;
+                    yield break;
+                }
+
+
+                if (!closedPath.Contains(newVec))
+                {
+                    pos newPos = new pos(newVec);
+                    newPos.parent = curPos;
+                    float GCost = curPos.GCost + Vector3.Distance(newVec, curPos.v);
+                    float Fcost = Vector3.Distance(end.v, newVec) + GCost;
+                    newPos.GCost = GCost;
+                    newPos.FCost = Fcost;
+                    newPos.parent = curPos;
+
+                    if (!activePathVectors.Contains(newVec))
+                    {
+                        while (activePath.ContainsKey(newPos.FCost))
+                        {
+                            //holy moly this is bad my god this needs to be fixed TODO TODO TODO
+                            newPos.FCost += .01f;
+                        }
+                        activePathVectors.Add(newPos.v);
+                        activePath.Add(newPos.FCost, newPos);
+                    }
+                }
+            }
+        }
+        if (activePath.Count == 0)
+        {
+            Debug.Log("path not found");
+            if (enemyMovement == null)
+            {
+                wallStorage.removeMostRecentWall();
+            }
+            else
+            {
+                yield return new WaitForSeconds(.5f);
+            }
+
+            towerPlacer.shadowTower.transform.position = new Vector3(25, 0, 0);
+            StartCoroutine(findPathBetweenPointsLyft(startVec, endVec, pathIndex));
+            yield break;
+        }
+    }
+
+    /*public IEnumerator findPathBetweenPointsFast(Vector3 startVec, Vector3 endVec, int pathIndex)
     {
         currentCount = 0;
         speedThreshold = 1;
@@ -211,7 +525,7 @@ public class Pathfinder : MonoBehaviour
             numCoroutinesRunning--;
             yield break;
         }
-    }
+    }*/
 
     public void findPath()
     {
@@ -241,7 +555,7 @@ public class Pathfinder : MonoBehaviour
         for (int i = 0; i < checkPointVectors.Count - 1; i++)
         {
             numCoroutinesRunning++;
-            StartCoroutine(findPathBetweenPointsFast(checkPointVectors[i], checkPointVectors[i + 1], i));
+            StartCoroutine(findPathBetweenPointsLyft(checkPointVectors[i], checkPointVectors[i + 1], i));
         }
     }
 
@@ -262,7 +576,7 @@ public class Pathfinder : MonoBehaviour
                         numCoroutinesRunning++;
                         if (this != null)
                         {
-                            StartCoroutine(findPathBetweenPointsFast(checkPointVectors[i], checkPointVectors[i + 1], i));
+                            StartCoroutine(findPathBetweenPointsLyft(checkPointVectors[i], checkPointVectors[i + 1], i));
                         }
                         break;
                     }
