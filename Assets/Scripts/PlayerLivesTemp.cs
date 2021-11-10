6212ccdaad5016feb3b6329418ffc3e9671001ce
greenlar <53580725+ArtGreenlee@users.noreply.github.com>
2021-11-10 11:55:54 -0500

    changed player movement controls!

--- Assets/Scripts/PlayerLivesTemp.cs
+++ Assets/Scripts/PlayerLivesTemp.cs
@@ -8,7 +8,8 @@ public class PlayerLivesTemp : MonoBehaviour
     public int numLives;
     public TextMeshProUGUI livesCounter;
     public static PlayerLivesTemp instance;
-   
+    private TowerInventory towerInventory;
+
 
 
     private void Awake()
@@ -20,8 +21,9 @@ public class PlayerLivesTemp : MonoBehaviour
     }
     private void Start()
     {
-
+        towerInventory = TowerInventory.instance;
     }
+
     private void Update()
     {
         if (numLives <= 0)
@@ -36,7 +38,12 @@ public class PlayerLivesTemp : MonoBehaviour
 
     public void loseLife()
     {
-        numLives--;
+        if (!towerInventory.debugMode)
+        {
+            numLives--;
+        }
+
+        
         livesCounter.text = "Lives " + numLives.ToString();
     }
 }
