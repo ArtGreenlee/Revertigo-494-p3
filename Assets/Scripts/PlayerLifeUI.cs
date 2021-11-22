0c328c55fb1ef946712e3933254a38b9d74d8db0
jasperscheiber <schjasp@umich.edu>
2021-11-21 23:19:05 -0500

    Life bar juice

--- Assets/Scripts/PlayerLifeUI.cs
+++ Assets/Scripts/PlayerLifeUI.cs
@@ -8,14 +8,36 @@ public class PlayerLifeUI : MonoBehaviour
 
     Subscription<PlayerLifeEvent> player_life_event_subscription;
 
+    public float k = 0.3f;
+    public float amplitude = .01f;
+    public float dampening_factor = 0.95f;
+    Vector3 velocity = Vector3.zero;
+    Vector3 origPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         player_life_event_subscription = EventBus.Subscribe<PlayerLifeEvent>(_OnPlayerLifeUpdated);
+        origPosition = transform.localPosition;
+    }
+
+    void Update(){
+        Vector3 displacement = origPosition - transform.localPosition;
+        Vector3 acceleration = k * displacement;
+        velocity += acceleration;
+        velocity *= dampening_factor;
+
+        transform.localPosition += velocity;
     }
 
     void _OnPlayerLifeUpdated(PlayerLifeEvent e){
         Debug.Log("Player life updated: " + e.delta_life);
+        if(e.delta_life < 0){
+            float randX = UnityEngine.Random.Range(-.5f, .5f) * Mathf.Abs(e.delta_life / 4f) * .000001f;
+            Vector3 randStart = new Vector3(randX, origPosition.y, origPosition.z);
+            transform.localPosition = randStart; 
+        }
+        
     }
 
     
--- Assets/Scripts/PlayerLivesTemp.cs
+++ Assets/Scripts/PlayerLivesTemp.cs
@@ -54,5 +54,6 @@ public class PlayerLivesTemp : MonoBehaviour
         //livesCounter.text = "Lives " + numLives.ToString();
         //Debug.Log(Mathf.Clamp01((float)numLives / max_lives));
         healthBar.value = Mathf.Clamp01((float)numLives / max_lives);
+        EventBus.Publish<PlayerLifeEvent>(new PlayerLifeEvent(-1));
     }
 }
