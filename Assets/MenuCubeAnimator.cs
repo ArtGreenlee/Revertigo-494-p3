06904b2ab498be0231ac905591fcb3f64696896a
Arthur Greenlee <greenzarthur@gmail.com>
2021-12-06 11:49:03 -0500

    playin wit da cube

--- Assets/MenuCubeAnimator.cs
+++ Assets/MenuCubeAnimator.cs
@@ -6,16 +6,19 @@ public class MenuCubeAnimator : MonoBehaviour
 {
     // Start is called before the first frame update
     bool up = false;
-    void Start()
+    Rigidbody rb;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.position.ToString());
-        transform.Rotate(Vector3.up* 6f * Time.deltaTime);
+        if (Time.frameCount % 360 == 0)
+        {
+            rb.AddTorque(Random.onUnitSphere * 3, ForceMode.Impulse);
+        }
+        
         if (!up && !(transform.position.y > 2)) {
             transform.position = new Vector3(transform.position.x, transform.position.y + 0.001f, transform.position.z);
         }
